5c759e765a112b3a62e055dd6170a828c5689d05
Kevin Cuadros <kev040798@gmail.com>
2021-06-12 21:33:08 -0400

    Test for retriving a single avatar

--- spec/requests/api/v1/avatar_request_spec.rb
+++ spec/requests/api/v1/avatar_request_spec.rb
@@ -21,4 +21,26 @@ describe 'Avatar API' do
     expect(avatar[:attributes][:weapons]).to be_a String
     expect(avatar[:attributes][:profession]).to be_a String
   end
+
+  it 'retrieves one avatar', :vcr do
+    avatar = AvatarFacade.all_avatars.first
+
+    get "/api/v1/avatars/#{avatar.id}"
+
+    expect(response).to be_successful
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    avatar = json[:data]
+    expect(avatar).to have_key :type
+    expect(avatar[:type]).to eq('avatar')
+
+    expect(avatar).to have_key :attributes
+    expect(avatar[:attributes].keys).to eq(%i[id name weapons profession])
+
+    expect(avatar[:attributes][:id]).to be_a String
+    expect(avatar[:attributes][:name]).to be_a String
+    expect(avatar[:attributes][:weapons]).to be_a String
+    expect(avatar[:attributes][:profession]).to be_a String
+  end
 end
