346601f9eb007b8ea23a3d43d05ef75c3d18439e
Kevin Cuadros <kev040798@gmail.com>
2021-06-14 20:28:40 -0400

    Save each search request being made

--- app/controllers/api/v1/session_controller.rb
+++ app/controllers/api/v1/session_controller.rb
@@ -2,6 +2,8 @@ module Api
   module V1
     class SessionController < ApplicationController
       def create
+        search = Search.new(request: params[:controller], action: params[:action], payload: payload.to_json)
+        search.save
         user = User.find_by(email: params[:email])
 
         if user && user.authenticate(params[:password])
@@ -9,6 +11,15 @@ module Api
         else
           render json: { error: 'Credentials are incorrect' }, status: 400
         end
+      end
+
+      private
+
+      def payload
+        {
+          'email' => params[:email],
+          'password' => params[:password]
+        }
       end
     end
   end
